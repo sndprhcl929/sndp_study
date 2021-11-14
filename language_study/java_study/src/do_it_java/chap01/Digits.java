8af448021629c1396b987d044cdfede4458a78d7
baekGeon <sndprhcl929@naver.com>
2021-11-14 21:35:53 +0900

    first commit

--- language_study/java_study/src/do_it_java/chap01/Digits.java
+++ language_study/java_study/src/do_it_java/chap01/Digits.java
@@ -1,19 +1,19 @@
 package chap01;
 import java.util.Scanner;
-// 2자리의 양수(10 ~ 99)를 입력합니다.
+// 2�옄由ъ쓽 �뼇�닔(10 ~ 99)瑜� �엯�젰�빀�땲�떎.
 
 class Digits {
 	public static void main(String[] args) {
 		Scanner stdIn = new Scanner(System.in);
 		int no;
 
-		System.out.println("2자리의 정수를 입력하세요.");
+		System.out.println("2�옄由ъ쓽 �젙�닔瑜� �엯�젰�븯�꽭�슂.");
 
 		do {
-			System.out.print("입력：");
+			System.out.print("�엯�젰竊�");
 			no = stdIn.nextInt();
 		} while (no < 10 || no > 99);
 
-		System.out.println("변수 no의 값은 " + no + "가(이) 되었습니다.");
+		System.out.println("蹂��닔 no�쓽 媛믪�� " + no + "媛�(�씠) �릺�뿀�뒿�땲�떎.");
 	}
 }
